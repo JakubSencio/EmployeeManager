b3208f9249c1b23de6b8e6e55b57e70b6c42b09b
Jacob <sencio.jakub@gmail.com>
2024-04-20 12:20:32 +0200

    y

--- EmployeeManager/Form1.cs
+++ EmployeeManager/Form1.cs
@@ -16,5 +16,10 @@ namespace EmployeeManager
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
